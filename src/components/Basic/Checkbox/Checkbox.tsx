0594a7bae3ae3319fbeead1efd0aeab82af1d354
MartinBozhilov-coh <martin.b@coherent-labs.com>
2025-04-28 17:54:38 +0300

    Add checkbox component and documentation (#21)
    * Add checkbox component and documentation
    
    * Expose the setter to the ref instead of the toggle function

---
+++ src/components/Basic/Checkbox/Checkbox.tsx
@@ -0,0 +1,86 @@
+import { createSlot, createSlots } from "@components/BaseComponent/Slots";
+import { ComponentProps } from "@components/types/ComponentProps";
+import { Accessor, Setter, createSignal, onMount, ParentComponent, ParentProps, Show } from "solid-js";
+import styles from './Checkbox.module.css';
+import { BaseComponent } from "@components/BaseComponent/BaseComponent";
+import CheckboxControl, { CheckboxSlotProps } from "./CheckboxControl";
+
+export interface CheckboxRef {
+    checked: Accessor<boolean>,
+    value: any,
+    setChecked: Setter<boolean>,
+    element: HTMLDivElement,
+}
+
+interface CheckBoxProps extends ComponentProps {
+    value?: any
+    checked?: boolean
+    disabled?: boolean
+    onChange?: (checked: boolean) => void;
+}
+
+interface LabelProps extends ParentProps  {
+    before?: boolean
+}
+
+const { useSlots, withSlots } = createSlots({
+    Label: createSlot<LabelProps>(),
+    Control: createSlot<CheckboxSlotProps>(),
+    Indicator: createSlot<CheckboxSlotProps>(),
+});
+
+const Checkbox: ParentComponent<CheckBoxProps> = (props) => {
+    const slots = useSlots();
+    const [checked, setChecked] = createSignal(props.checked ?? false);
+    const isBefore = slots.Label?.before;
+    let element!: HTMLDivElement;
+
+    props.componentClasses = `${styles.Checkbox} ${props.disabled ? styles.Disabled : ''}`;
+ 
+    const toggle = (e?: MouseEvent) => {
+        if (props.disabled) return;
+
+        setChecked(prev => !prev);
+        props.click?.(e as MouseEvent);
+        props.onChange?.(checked())
+    }
+
+    onMount(() => {
+        if (!props.ref || !element) return;
+
+        (props.ref as unknown as (ref: any) => void)({
+            checked,
+            value: props.value,
+            setChecked,
+            element,
+        });
+    });
+
+    return (
+        <div
+            ref={element!}
+            {...BaseComponent(props).attributes}
+            {...BaseComponent(props).eventHandlers} 
+            class={BaseComponent(props).className}
+            style={BaseComponent(props).style}
+            onclick={toggle}>
+
+            <Show when={isBefore}>
+                {slots.Label?.children}
+            </Show>
+
+            <CheckboxControl controlSlot={slots.Control} indicatorSlot={slots.Indicator} before={isBefore} checked={checked} />
+
+            <Show when={!slots.Label}>
+                {props.children}
+            </Show>
+
+            <Show when={!isBefore}>
+                {slots.Label?.children}
+            </Show>
+
+        </div>
+    )
+}
+
+export default withSlots(Checkbox)
