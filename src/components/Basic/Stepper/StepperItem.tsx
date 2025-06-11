77b2eb5cf480fa74cd4259ff3126f32a1e16dcd5
Kaloyan Geshev <kaloyan.g@coherent-labs.com>
2025-06-11 17:24:54 +0300

    Add stepper component (#28)
    * Added stepper component
    
    * Documented stepper component
    
    * Review issues fixed

---
+++ src/components/Basic/Stepper/StepperItem.tsx
@@ -0,0 +1,41 @@
+import { JSX, onCleanup, onMount, ParentComponent, ParentProps, Show, useContext } from "solid-js";
+import styles from './Stepper.module.css';
+
+import { createTokenComponent } from "@components/utils/tokenComponents";
+import { StepperContext } from "./Stepper";
+import Flex from "@components/Layout/Flex/Flex";
+
+interface ItemTokenProps {
+    style?: JSX.CSSProperties,
+    class?: string,
+    'class-selected'?: string
+    selected?: boolean
+    value: string
+}
+
+export const Item = createTokenComponent<ItemTokenProps>();
+
+interface StepperItempProps {
+    item: ParentProps<ItemTokenProps>
+}
+
+export const StepperItem: ParentComponent<StepperItempProps> = (props) => {
+    const stepperContext = useContext(StepperContext);
+    onMount(() => {
+        stepperContext?.registerOption(props.item.value, props.item.selected);
+    })
+
+    onCleanup(() => {
+        stepperContext?.unregisterOption(props.item.value);
+    })
+
+    return (
+        <Show when={stepperContext?.selected() === props.item.value}>
+            <div
+                class={`${styles.StepperItem} ${props.item.class || ''}`}
+                style={props.item.style || {}}>
+                {props.item.children}
+            </div>
+        </Show>
+    )
+}
