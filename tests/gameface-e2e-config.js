dcca4bb825f38057b30f3e42b87c4fd2954b9b8c
MartinBozhilov-coh <martin.b@coherent-labs.com>
2025-06-24 17:55:28 +0300

    Adjust e2e config

--- tests/gameface-e2e-config.js
+++ tests/gameface-e2e-config.js
@@ -1,5 +1,4 @@
 module.exports = {
-    // tests: "tests/**/*.spec.js",
-    tests: "tests/playground/base.spec.js",
+    tests: "tests/**/*.spec.js",
     gamefacePath: "C:/Users/Martin.B/Desktop/Players/Gameface-1.64.1.4-Pro/Player/Player.exe"
 }
