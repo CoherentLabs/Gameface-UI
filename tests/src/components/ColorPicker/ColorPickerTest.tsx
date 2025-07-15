e4e0880923203fed01f76e7ea449ccc82170ff12
Kaloyan Geshev <kaloyan.g@coherent-labs.com>
2025-07-15 14:17:15 +0300

    Add color picker (#39)
    * Added color picker
    
    * Added tests for the color picker component
    
    * Refactored index page of the components to dynamically load svg preview files
    
    * Documented color picker component

---
+++ tests/src/components/ColorPicker/ColorPickerTest.tsx
@@ -0,0 +1,55 @@
+import Tab from "@components/Layout/Tab/Tab";
+import { createMemo, createSignal, For, onCleanup, onMount } from "solid-js";
+import selectors from "../../../shared/color-picker-selectors.json";
+import ColorPicker, { ColorPickerRef } from "@components/Complex/ColorPicker/ColorPicker";
+import { parseHSVAColor, RGBAToHSVA } from "@components/Complex/ColorPicker/colorPickerUtils";
+
+const initialValue = 'rgba(255, 0, 0, 1)';
+const ColorPickerTest = () => {
+    let colorPickerRef!: ColorPickerRef;
+    const [value, setValue] = createSignal(initialValue);
+    const [test, setTest] = createSignal('red');
+
+    const scenarios = [
+        { label: "Change value with ref", action: () => { colorPickerRef.changeColor({ h: 50, s: 50, v: 50, a: 1 }) } },
+        { label: "Change styles", action: () => { setTest('blue') } },
+    ];
+
+    const reset = () => {
+        setValue(initialValue);
+        setTest('red');
+        colorPickerRef?.changeColor(RGBAToHSVA(initialValue));
+    };
+
+    const isReactive = createMemo(() => test() === 'blue');
+    const reactiveClass = createMemo(() => isReactive() ? 'reactive' : '');
+    const reactiveStyle = createMemo(() => isReactive() ? { 'background-color': 'blue' } : {});
+
+    onMount(() => document.addEventListener('reset', reset))
+    onCleanup(() => document.removeEventListener('reset', reset))
+
+    return (
+        <Tab location='color-picker'>
+            <div class={selectors.assertionElement}>{value()}</div>
+
+            <For each={scenarios}>
+                {(sc, i) => (
+                    <button class={`${selectors.scenarioBtn} scenario-${i()}`} onClick={sc.action} >
+                        {sc.label}
+                    </button>
+                )}
+            </For>
+
+            <ColorPicker
+                ref={colorPickerRef}
+                //@ts-ignore
+                onChange={(value) => setValue(parseHSVAColor(value).rgba)}
+                value={initialValue}
+                style={reactiveStyle()}
+                class={`${selectors.colorPicker} ${reactiveClass()}`}>
+            </ColorPicker>
+        </Tab>
+    )
+}
+
+export default ColorPickerTest;
