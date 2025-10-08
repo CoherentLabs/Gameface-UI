5153b4fda63f658693ba20882414ffdb04654b35
MartinBozhilov-coh <martin.b@coherent-labs.com>
2025-10-08 13:31:45 +0300

    Progress components (#56)
    * Implement progress bar and circle components
    
    * Add tests and fix es lint bug
    
    * Add documentation
    
    * Rebase, add pr suggestions, fix toast tests
    
    * bump version
    
    * add warning in the docs for potential ver mismatch

---
+++ src/components/Feedback/Progress/ProgressBar.tsx
@@ -0,0 +1,41 @@
+import { createEffect, createMemo, onMount, ParentComponent } from "solid-js";
+import { createTokenComponent, TokenBase, useToken } from "@components/utils/tokenComponents";
+import useBaseComponent from "@components/BaseComponent/BaseComponent";
+import { clampProgress, ProgressProps } from "./Progress";
+import styles from './Progress.module.scss';
+
+const Fill = createTokenComponent<TokenBase>();
+const ProgressBar: ParentComponent<ProgressProps> = (props) => {
+    const fillToken = useToken(Fill, props.children)
+    let fillElement: HTMLDivElement | undefined;
+
+    createEffect(() => {
+        if (fillElement) fillElement.style.width = `${clampProgress(props.progress)}%`;
+    })
+    
+    const fillClasses = createMemo(() => {
+        const classes = [styles['bar-fill']];
+        classes.push(fillToken()?.class || "");
+
+        return classes.join(' ');
+    });
+
+    props.componentClasses = () => styles.bar;
+    const { className, inlineStyles, forwardEvents, forwardAttrs } = useBaseComponent(props);
+
+    return (
+        <div
+            ref={props.ref as HTMLDivElement}
+            class={className()}
+            style={inlineStyles()}
+            use:forwardEvents={props}
+            use:forwardAttrs={props}>
+            <div 
+                ref={fillElement}
+                class={fillClasses()} 
+                style={fillToken()?.style} />
+        </div>
+    )
+}
+
+export default Object.assign(ProgressBar, { Fill })
---
+++ tests/specs/progress/progress-bar.spec.js
@@ -0,0 +1,66 @@
+const assert = require('assert');
+const selectors = require('../../shared/progress-selectors.json');
+
+describe('Progress Bar', function () {
+    this.beforeAll(async () => {
+        await gf.navigate(`http://localhost:3000/components-e2e/`);
+        await gf.sleep(1000);
+        await gf.click('.progress-bar-link');
+    })
+
+    this.afterEach(async () => {
+        await gf.trigger('reset');
+    })
+
+    it('Should render itself and it\'s children correctly', async () => {
+        const progress= await gf.get(`.${selectors.base}`);
+        const fill= await progress.find(`.${selectors.fill}`);
+
+        assert.ok(progress, 'Progress bar should be in the DOM');
+        assert.ok(fill, 'Progress bar fill should be in the DOM');
+    })
+    
+    it('Should load to 100%', async () => {
+        const fill= await gf.get(`.${selectors.fill}`);
+        await gf.click(`.${selectors.scenarioBtn}.scenario-0`);
+
+        const hasStyle = await fill.waitForStyles({ width: '100%' });
+        assert(hasStyle);
+    })
+
+    it('Should load to 50%', async () => {
+        const fill= await gf.get(`.${selectors.fill}`);
+        await gf.click(`.${selectors.scenarioBtn}.scenario-1`);
+
+        const hasStyle = await fill.waitForStyles({ width: '50%' });
+        assert(hasStyle);
+    })
+
+    it('Should not load beyond 100%', async () => {
+        const fill= await gf.get(`.${selectors.fill}`);
+        await gf.click(`.${selectors.scenarioBtn}.scenario-2`);
+
+        await gf.sleep(1000)
+        const {width} = await fill.styles();
+        assert.notEqual(width, 120, 'Width should be 100%');
+    })
+
+    describe('reactivity', () => {
+        const scenarios = [
+            { selector: `.${selectors.base}`, desc: 'base' },
+            { selector: `.${selectors.fill}`, desc: 'fill' },
+        ];
+
+        for (const { selector, desc } of scenarios) {
+            it(`should update styles & classes reactively on props change — ${desc}`, async () => {
+                await gf.click(`.${selectors.scenarioBtn}.scenario-3`);
+                const el = await gf.get(selector);
+                const styles = await el.styles();
+                const classes = await el.classes();
+
+                assert.ok(classes.includes('reactive'), 'class "reactive" applied');
+                assert.equal(styles['background-color'], 'rgba(0, 0, 255, 1)', 'styles update');
+            });
+        };
+    });
+});
