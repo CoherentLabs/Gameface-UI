01a2a5f88bd85b49adb7a392c6b81244795c770f
MartinBozhilov-coh <martin.b@coherent-labs.com>
2026-03-13 17:34:35 +0200

    Add gap support to flex and grid components.

--- src/components/Layout/Flex/Flex.tsx
+++ src/components/Layout/Flex/Flex.tsx
@@ -1,7 +1,8 @@
-import { ParentComponent } from "solid-js";
+import { createMemo, onMount, ParentComponent } from "solid-js";
 import LayoutBase from "../LayoutBase";
 import styles from './Flex.module.scss'
 import { ComponentBaseProps } from "../../types/ComponentProps";
+import { GAMEFACE_VERSION, verIsAtLeast } from "@components/utils/gamefaceVersion";
 
 interface FlexProps extends ComponentBaseProps {
     direction?: 'row' | 'row-reverse' | 'column' | 'column-reverse';
@@ -9,22 +10,47 @@ interface FlexProps extends ComponentBaseProps {
     'justify-content'?: 'start' | 'center' | 'end' | 'space-between' | 'space-around';
     'align-items'?: 'start' | 'end' | 'center' | 'stretch';
     'align-content'?: 'start' | 'end' | 'center' | 'stretch';
+    gap?: string;
+    'row-gap'?: string;
+    'column-gap'?: string;
 }
 
-const getFlexProperties = (props: FlexProps) => {
-    let flexClasses = [styles.flex];
-
-    if (props.direction) flexClasses.push(styles[`flex-${props.direction}`]);
-    if (props.wrap) flexClasses.push(styles[`flex-${props.wrap}`]);
-    if (props['justify-content']) flexClasses.push(styles[`flex-justify-${props['justify-content']}`]);
-    if (props['align-items']) flexClasses.push(styles[`flex-align-${props['align-items']}`]);
-    if (props['align-content']) flexClasses.push(styles[`flex-content-${props['align-content']}`]);
-
-    return flexClasses
-}
+let warningShown = false;
 
 const Flex: ParentComponent<FlexProps> = (props) => {
-    return <LayoutBase {...props} componentClasses={getFlexProperties(props).join(' ')} />
+    onMount(() => {
+        const hasGap = !!(props.gap || props["row-gap"] || props["column-gap"]);
+        if (!warningShown && hasGap && !verIsAtLeast(2, 2)) {
+            console.warn(
+                `[Gameface UI] The "gap" property is unsupported in Gameface v${GAMEFACE_VERSION}. Upgrade to 2.2+`
+            );
+            warningShown = true;
+        }
+    });
+
+    const classes = createMemo(() => {
+        const flexClasses = [styles.flex];
+        if (props.direction) flexClasses.push(styles[`flex-${props.direction}`]);
+        if (props.wrap) flexClasses.push(styles[`flex-${props.wrap}`]);
+        if (props['justify-content']) flexClasses.push(styles[`flex-justify-${props['justify-content']}`]);
+        if (props['align-items']) flexClasses.push(styles[`flex-align-${props['align-items']}`]);
+        if (props['align-content']) flexClasses.push(styles[`flex-content-${props['align-content']}`]);
+        return flexClasses.join(' ');
+    });
+
+    const gapStyles = createMemo(() => ({
+        gap: props.gap,
+        'row-gap': props['row-gap'],
+        'column-gap': props['column-gap'],
+    }));
+
+    return (
+        <LayoutBase 
+            {...props} 
+            componentClasses={classes()}
+            componentStyles={gapStyles()} 
+        />
+    );
 }
 
 export default Flex
---
+++ src/components/utils/gamefaceVersion.ts
@@ -0,0 +1,17 @@
+const versionMatch = /Cohtml\/([\d.]+)/.exec(navigator.userAgent);
+const rawVersion = versionMatch ? versionMatch[1] : "0.0.0.0";
+
+const VERSION_PARTS = rawVersion.split('.').map(Number);
+export const GAMEFACE_VERSION = rawVersion;
+
+/**
+ * Checks if the current version satisfies a minimum requirement.
+ * Example: isAtLeast(2, 2) returns true if version is 2.2.0 or 2.3.0
+ */
+export function verIsAtLeast(major: number, minor: number = 0, patch: number = 0): boolean {
+  const [currMajor, currMinor, currPatch] = VERSION_PARTS;
+
+  if (currMajor !== major) return currMajor > major;
+  if (currMinor !== minor) return currMinor > minor;
+  return currPatch >= patch;
+}
