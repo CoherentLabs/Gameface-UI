3c5d0c2bf6e5a765d7d2b67cfe29a7b52f8396af
Mihail Todorov <mihail@coherent-labs.com>
2026-07-15 16:41:19 +0300

    Fix astro config

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -3,9 +3,9 @@ import solid from '@astrojs/solid-js';
 import solidSvg from 'vite-plugin-solid-svg';
 import { defineConfig } from 'astro/config';
 import starlightLinksValidator from 'starlight-links-validator';
-import coherentTheme from 'coherent-docs-theme'
+import { pluginCollapsibleSections } from '@expressive-code/plugin-collapsible-sections';
+import coherentTheme from 'coherent-docs-theme';
 import starlightSidebarTopics from 'starlight-sidebar-topics';
-import coherentTheme, { generateDocsChangelog, generateVersionWithPackageJSON } from 'coherent-docs-theme';
 import path from 'node:path';
 import { fileURLToPath } from 'url';
 import { componentSidebarItems, recipesSidebarItems } from './src/config/sidebarItems';
@@ -59,13 +59,41 @@ import { remarkGfuiDemo } from './src/components/CodePreviewGFUI/plugins/remark-
 
 /** @type {import('@astrojs/starlight/expressive-code').StarlightExpressiveCodeOptions} */
 export default defineConfig({
+  markdown: {
+    remarkPlugins: [remarkGfuiDemo],
+  },
   vite: {
+    plugins: [
+      gfuiDemoPlugin(),
+      solidSvg({
+        defaultAsComponent: false,
+        svgo: { enabled: false },
+      }),
+    ],
+    build: {
+      cssCodeSplit: true,
+      minify: false,
+    },
     resolve: {
       preserveSymlinks: true,
+      dedupe: ['solid-js', 'solid-js/web', 'solid-js/store'],
       alias: {
-        '@components': path.join(__dirname, "src", "components"),
-      }
-    }
+        '@components': path.resolve(__dirname, '../src/components'),
+        '@custom-components': path.resolve(__dirname, '../src/custom-components'),
+        '@assets': path.resolve(__dirname, '../src/assets'),
+        '@docs-components': path.resolve(__dirname, './src/components'),
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+          @use '@assets/scss/variables' as *;
+        `,
+          api: 'modern',
+        },
+      },
+    },
   },
   integrations: [
     starlight({
@@ -73,7 +101,7 @@ export default defineConfig({
         plugins: [pluginCollapsibleSections()],
       },
       components: {
-        Sidebar: '@components/CustomSidebar.astro',
+        Sidebar: '@docs-components/CustomSidebar.astro',
       },
       favicon: '/favicon-32x32.png',
       credits: false,
@@ -91,34 +119,10 @@ export default defineConfig({
         starlightLinksValidator(),
       ],
       social: [
-    markdown: {
-        remarkPlugins: [remarkGfuiDemo],
-    },
-    vite: {
-        plugins: [
-            gfuiDemoPlugin(),
-            solidSvg({
-                defaultAsComponent: false,
-                svgo: { enabled: false },
-            }),
-        ],
-        build: {
-            cssCodeSplit: true,
-            minify: false, // for easier debugging of emitted demo chunks and manifest
-        },
         {
           icon: 'seti:github',
           label: 'Git',
           href: 'https://github.com/CoherentLabs/Gameface-UI',
-        resolve: {
-            preserveSymlinks: true,
-            dedupe: ['solid-js', 'solid-js/web', 'solid-js/store'], // ensure singletons of Solid packages
-            alias: {
-                '@components': path.resolve(__dirname, '../src/components'),
-                '@custom-components': path.resolve(__dirname, '../src/custom-components'),
-                '@assets': path.resolve(__dirname, '../src/assets'),
-                '@docs-components': path.resolve(__dirname, './src/components'),
-            },
         },
         {
           icon: 'laptop',
@@ -133,64 +137,7 @@ export default defineConfig({
       ],
       title: 'GamefaceUI',
     }),
+    solid(),
   ],
   site: 'https://gameface-ui.coherent-labs.com',
 })
-        css: {
-            preprocessorOptions: {
-                scss: {
-                    additionalData: `
-          @use '@assets/scss/variables' as *;
-        `,
-                    api: 'modern',
-                },
-            },
-        },
-    },
-    integrations: [
-        starlight({
-            expressiveCode: {},
-            favicon: '/favicon-32x32.png',
-            credits: false,
-            customCss: ['./src/styles/custom.css'],
-            plugins: [
-                ...coherentTheme({
-                    documentationSearchTag: 'Gameface UI',
-                }),
-                starlightLinksValidator(),
-            ],
-            sidebar: [
-                await generateVersionWithPackageJSON('../package.json', 'https://github.com/CoherentLabs/Gameface-UI'),
-                {
-                    label: 'Getting Started',
-                    autogenerate: { directory: 'getting-started' },
-                },
-                {
-                    label: 'Concepts',
-                    autogenerate: { directory: 'concepts' },
-                },
-                {
-                    label: 'Components',
-                    collapsed: true,
-                    autogenerate: { directory: 'components', collapsed: false },
-                },
-                generateDocsChangelog(path.join(__dirname, `./src/content/docs/changelog/index.mdx`)),
-            ],
-            social: [
-                {
-                    icon: 'laptop',
-                    label: 'Site',
-                    href: 'https://coherent-labs.com/',
-                },
-                {
-                    icon: 'email',
-                    label: 'Email',
-                    href: 'https://coherent-labs.com/get-in-touch',
-                },
-            ],
-            title: 'GamefaceUI',
-        }),
-        solid(),
-    ],
-    site: 'https://gameface-ui.coherent-labs.com',
-});
