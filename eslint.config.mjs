5e032681d6ae2492abb165c55a5717c116e3c648
MihailTodorov <74198607+MihailTodorov@users.noreply.github.com>
2026-07-22 18:11:24 +0300

    Add eslint to the boilerplate (#80)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,25 @@
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
-import tokensValidator from "./eslint-custom-rules/tokens-validator.js";
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+import tokensValidator from './eslint-custom-rules/tokens-validator.js';
+import gameface from 'eslint-plugin-gameface';
 
 export default defineConfig([
-  {
-    files: ["**/*.tsx"],
-    plugins: {
-      custom: {
-        rules: {
-          "tokens-validator": tokensValidator,
+    {
+        ignores: ['**/node_modules/**', 'dist/**', 'docs/**', 'tests/**', '**/**.html'],
+    },
+    ...gameface.configs['flat/recommended'],
+    {
+        files: ['**/*.tsx'],
+        plugins: {
+            custom: {
+                rules: {
+                    'tokens-validator': tokensValidator,
+                },
+            },
         },
-      },
+        rules: {
+            'custom/tokens-validator': 'warn',
+        },
     },
-    rules: {
-      "custom/tokens-validator": "warn",
-    },
-  },
-  tseslint.configs.base,
+    tseslint.configs.base,
 ]);
